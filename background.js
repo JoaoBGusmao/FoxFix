aafa317c48c06eae5c658b14d3198ae88041013b
João Gusmão <joao.neto@escale.com.br>
2018-02-22 19:51:44 -0300

    Using observer instead of loop

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
 // Autor: João Gusmão - http://github.com/joaobgusmao
 
-setInterval(
-    function() {
-        var price = document.querySelector('[data-model-key-list="formatted_BLINK_BTCBRL_LAST_PX"]').innerHTML;
-        document.title = price + " Compre e venda Bitcoins no Brasil";
-    }, 1
-)
+var target = document.querySelector('title');
+var lastTitle = '';
+
+var observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        if(lastTitle != document.title || lastTitle == '') {
+            var price = document.querySelector('[data-model-key-list="formatted_BLINK_BTCBRL_LAST_PX"]').innerHTML;
+            var newTitle = price + " Compre e venda Bitcoins no Brasil";
+            document.title = newTitle
+            lastTitle = newTitle;
+        }
+    });
+});
+
+var config = { attributes: true, childList: true, characterData: true }
+
+observer.observe(target, config);
